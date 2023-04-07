3dcd7a4107d1c83fc05883a5c381de02cd865b75
Aladser <aladser@gmail.com>
2023-04-07 11:34:10 +0900

    рфкт класса модели таблицы

--- engine/table_models/TableDBModel.php
+++ engine/table_models/TableDBModel.php
@@ -2,35 +2,9 @@
 
 // Класс модели таблицы БД
 class TableDBModel{
-    protected $dbConnection; // соединение с БД
-    protected $host;
-    protected $nameDB;
-    protected $userDB;
-    protected $passwordDB;
     protected $db;
 
-    function __construct($host, $nameDB, $userDB, $passwordDB, $db){
-        $this->host = $host;
-        $this->nameDB = $nameDB;
-        $this->userDB = $userDB;
-        $this->passwordDB = $passwordDB;
-
+    function __construct($db){
         $this->db = $db;
-    }
-
-    protected function connect(){
-        try{
-            $dbname = $this->nameDB;
-            $host = $this->host;
-            $this->dbConnection = new PDO("mysql:dbname=$dbname; host=$host", $this->userDB, $this->passwordDB, 
-            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-        }
-        catch(PDOException $e){
-            die($e->getMessage());
-        }
-    }
-
-    protected function disconnect(){
-        $this->dbConnection = null;
     }
 }
